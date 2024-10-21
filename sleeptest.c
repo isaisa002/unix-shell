e5324a08b58d03edd1a9e3ef34c8aba0ed9d9382
isaisa002 <ketisadona@gmail.com>
2024-10-21 18:28:23 -0400

    files for demo added

--- sleeptest.c
+++ sleeptest.c
@@ -2,21 +2,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(int argument_count, char *arguments[]) {
-    int sleep_duration = 60; // Default sleep duration is 60 seconds
+int main(int argc, char *argv[]) {
+	
+    int sleep_time = 40;  // Default sleep time 40 seconds
 
-    // An argument was given by user as the duration of sleep
-    if (argument_count > 1) {
-        sleep_duration = atoi(arguments[1]); // Convert the argument to an integer
-        if (sleep_duration <= 0) {
-            fprintf(stderr, "Invalid duration. Please provide a positive integer.\n");
-            return 1;
-        }
+    // If an argument is passed, use it as the sleep time
+    if (argc == 2) {
+        sleep_time = atoi(argv[1]);
     }
 
-    printf("Command entered: Sleeping for %d seconds\n", sleep_duration);
-    sleep(sleep_duration); // Sleep for the specified duration
-    printf("Finished sleeping for %d seconds.\n", sleep_duration);
+    printf("Sleeping for %d seconds...\n", sleep_time);
+    sleep(sleep_time);
+    printf("Woke up after %d seconds\n", sleep_time);
 
     return 0;
 }
